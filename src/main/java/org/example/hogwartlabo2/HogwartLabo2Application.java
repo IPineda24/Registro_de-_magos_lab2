fe3261f2b998a6608e1a044069e86088052db272
Irvin Pineda <00024420@uca.edu.sv>
2026-05-06 19:36:40 -0600

    Start project :tada:

---
+++ src/main/java/org/example/hogwartlabo2/HogwartLabo2Application.java
@@ -0,0 +1,13 @@
+package org.example.hogwartlabo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartLabo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartLabo2Application.class, args);
+    }
+
+}
